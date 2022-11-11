d500130c4c4d2643c641423eab6dba14a280f515
zhangtianyuan <tianyuan233.zhang@gmail.com>
2022-11-11 17:27:14 +0800

    变更

--- src/main/java/com/tianyuan/demo/controller/HelloWorld.java
+++ src/main/java/com/tianyuan/demo/controller/HelloWorld.java
@@ -1,19 +1,13 @@
 package com.tianyuan.demo.controller;
 
 
-import com.tianyuan.demo.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class Helloworld {
+public class HelloWorld {
     @GetMapping("/hello")
     public String hello(@RequestParam("nickname") String name) {
         return "hello " + name;
-    }
-
-    @GetMapping("/user/{id}")
-    public User user(@PathVariable int id) {
-        return new User("Qwe");
     }
 
 }
